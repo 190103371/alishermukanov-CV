17362d8f6aac8c9960bae444f18f34f6b40a7984
alishermv <naiman.team@gmail.com>
2021-02-22 21:01:29 +0600

    initial commit

---
+++ resources/views/newpage.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>New Page</title>
+</head>
+<body>
+	<p>This is second page</p>
+    <a href=" {{ asset('/') }}">Main Page</a>
+</body>
+</html>
